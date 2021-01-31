c52532ed2e79800ddefa09d51e25d08ff669daae
ryo <ryokokeguchi@gmail.com>
2021-01-31 21:45:16 +0900

    二章開始

--- Chapter1/NumPira.java
+++ Chapter1/NumPira.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-class Piramid {
-    static void spira(int n) {
+class NumPira {
+    static void npira(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++)
                 System.out.print(" ");
             for (int j = 1; j <= (i - 1) * 2 + 1; j++)
-                System.out.print('*');
+                System.out.print(i % 10);
             System.out.println();
         }
     }
@@ -14,11 +14,11 @@ class Piramid {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         int n;
-        System.out.println("ピラミッドを表示する");
+        System.out.print("ピラミッドを表示します");
         do {
-            System.out.print("段数は");
+            System.out.print("値段");
             n = stdIn.nextInt();
         } while (n <= 0);
-        spira(n);
+        npira(n);
     }
 }
